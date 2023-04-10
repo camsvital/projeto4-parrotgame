f3b022eeab5093ccf7834c113a9ca38049d6a719
Camila Vital <camsvital@gmail.com>
2023-04-09 21:22:51 -0300

    adicionando divs

--- script.js
+++ script.js
@@ -3,41 +3,41 @@ let playCount = 0;
 let pars = 0;
 let cardSequence = [];
 
+function game() {
+  qtdCards = prompt("Com quantas cartas você quer jogar?");
 
+  if ((qtdCards < 4, qtdCards > 14, qtdCards % 2 !== 0)) {
+    prompt("Com quantas cartas você quer jogar?");
+  } else {
+    for (let i = 1; i < qtdCards / 2; i++) {
+      cardSequence[2 * (i - 1)] = i;
+      cardSequence[2 * (i - 1) + 1] = i;
+    }
 
-function game(){
-    
-    qtdCards = prompt('Com quantas cartas você quer jogar?')
+    const content = document.querySelector(".content");
+    conteudo.innerHTML = "";
+    for (let i = 1; i < qtdCards; i++) {
+      let text = "";
 
-    if (qtdCards<4, qtdCards>14, qtdCards%2!==0){
-        prompt('Com quantas cartas você quer jogar?')
-    }else{
-    const for(let i = 1; i< qtdCads/2; i++){
-    let text ="";
-
-    switch(cardSequence[i - 1]){
+      switch (cardSequence[i - 1]) {
         case 1:
-            texto = '<div class="grid"/>
+          texto = `<div class="grid"/>
             <div>
               <img src="img/back.png" />
-            </div>';
-            
-            break;
-            
+            </div>`;
+
+          break;
+      }
     }
-    }
-    }
+  }
 }
 
 game();
 
 cardSequence.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
 
-function distribuirCard(){
+function distribuirCard() {}
 
-    
-}
-
-function comparador() { 
-	return Math.random() - 0.5;
+function comparador() {
+  return Math.random() - 0.5;
 }
